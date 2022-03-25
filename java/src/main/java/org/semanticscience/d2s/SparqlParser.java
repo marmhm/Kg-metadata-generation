98dead3ae19203da5712359c67a613f905b98136
Maryam <default@maastrichtuniversity.nl>
2022-03-25 11:03:32 +0000

    updated
    Former-commit-id: 26b4a701cb9e7713858b82a71e7eca9ffa814840

--- java/src/main/java/org/semanticscience/d2s/SparqlParser.java
+++ java/src/main/java/org/semanticscience/d2s/SparqlParser.java
@@ -27,21 +27,24 @@ public class SparqlParser {
 
 	public static void main(String[] args) throws Exception {
 		try { 
-                  String fileName = "pattern.json";
+                  String fileName = "nodespatterns.txt";
                   Path filePath = Path.of(fileName);
                   // String text = "Welcome to geekforgeeks\nHappy Learning!";
                   // Files.writeString(filePath, text);
                   String file_content = Files.readString(filePath);
-                  System.out.println(file_content);
+                  // System.out.println(file_content);
                   // Files.write(stateFile.toPath(), content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
                   System.out.println("Parsing SPARQL queries with RDF4J:");
                   //String sparqlQuery = "SELECT * WHERE { ?s ?p ?o . ?s a ?type.   . FILTER(?type = <http://ohoho>) . } LIMIT 14";
 
-                  String sparqlQueries = Resources.toString(Resources.getResource("query1.rq"), StandardCharsets.UTF_8);
+                  String sparqlQueries = Resources.toString(Resources.getResource("wholequery.rq"), StandardCharsets.UTF_8);
                   
                   SPARQLParserFactory factory = new SPARQLParserFactory();
                   QueryParser parser = factory.getParser();
                   // Path fileName1 = Path.of("patterns.json");
+                  
+                  
+                  JSONArray allQueries = new JSONArray();
                   for (String sparqlQuery : sparqlQueries.split("##########")) {
                         // System.out.println(sparqlQuery);
                         ParsedQuery parsedQuery = parser.parseQuery(sparqlQuery, null);
@@ -50,40 +53,86 @@ public class SparqlParser {
                         TupleExpr tupleExpr = parsedQuery.getTupleExpr();
                         tupleExpr.visit(collector);
 
-                        JSONArray arr = new JSONArray();
+                        // JSONArray arr = new JSONArray();
                         for (StatementPattern pattern : collector.getStatementPatterns()) {
-                              System.out.println("### NEW STATEMENT");
-                              System.out.println("Subject (name/value/class):");
-                              System.out.println(pattern.getSubjectVar().getName());
-                              System.out.println(pattern.getSubjectVar().getValue());
-                              System.out.println(pattern.getSubjectVar().getClass());
-                              System.out.println("");
-                              System.out.println("Predicate:");
-                              System.out.println(pattern.getPredicateVar().getName());
-                              System.out.println(pattern.getPredicateVar().getValue());
-                              System.out.println(pattern.getPredicateVar().getClass());
-                              System.out.println("");
-                              System.out.println("Object:");
-                              System.out.println(pattern.getObjectVar().getName());
-                              System.out.println(pattern.getObjectVar().getValue());
-                              System.out.println(pattern.getObjectVar().getClass());
-                              System.out.println("");
+                              // System.out.println("Predicate:");
+                              // System.out.println(pattern.getFil);
+                              // System.out.println(pattern.getPredicateVar().getValue());
+                              // System.out.println(pattern.getPredicateVar().getClass());
+                              // System.out.println("");
+                              // System.out.println("### NEW STATEMENT");
+                              // System.out.println("Subject (name/value/class):");
+                              // System.out.println(pattern.getSubjectVar().getName());
+                              // System.out.println(pattern.getSubjectVar().getValue());
+                              // System.out.println(pattern.getSubjectVar().getClass());
+                              // System.out.println("");
+                              // System.out.println("Predicate:");
+                              // System.out.println(pattern.getPredicateVar().getName());
+                              // System.out.println(pattern.getPredicateVar().getValue());
+                              // System.out.println(pattern.getPredicateVar().getClass());
+                              // System.out.println("");
+                              // System.out.println("Object:");
+                              // System.out.println(pattern.getObjectVar().getName());
+                              // System.out.println(pattern.getObjectVar().getValue());
+                              // System.out.println(pattern.getObjectVar().getClass());
+                              // System.out.println("");
                               // System.out.println("Context:");
                               // System.out.println(pattern.getContextVar().getName());
                               // System.out.println(pattern.getContextVar().getValue());
                               // System.out.println(pattern.getContextVar().getClass());
                               // System.out.println("");
+                              //for creating group pattern we creat json
 
-                              JSONObject obj = new JSONObject();
-                              obj.put("subjectName", pattern.getSubjectVar().getName().toString());
-                              obj.put("predicateName", pattern.getPredicateVar().getName().toString());
-                              obj.put("objectName", pattern.getObjectVar().getName().toString());
-                              arr.add(obj);
-                        
-                        }
-                        // Files.writeString(fileName1, text1);}
-                        Files.write(Paths.get(fileName), arr.toJSONString().getBytes(), StandardOpenOption.APPEND);
-                  }
+                  //             JSONObject obj = new JSONObject();
+                  //             obj.put("subjectValue", pattern.getSubjectVar().getValue());
+                  //             obj.put("predicateValue", pattern.getPredicateVar().getValue());
+                  //             obj.put("objectValue", pattern.getObjectVar().getValue());
+                  //             arr.add(obj);
+                  //       }
+                  //       allQueries.add(arr);
+                  // }
+                  // Files.writeString(fileName1, text1);}
+                  // Files.write(Paths.get(fileName), allQueries.toJSONString().getBytes(), StandardOpenOption.APPEND);
+                              String b;
+                              String bb;
+                              String bbb;
+                              String bbbb;
+                              String bbbm;
+                              if(pattern.getPredicateVar().getValue()!= null && pattern.getSubjectVar().getValue()!= null && pattern.getObjectVar().getValue()!= null){
+                                  b= pattern.getPredicateVar().getValue()+"\r\n";
+                                  bb= pattern.getSubjectVar().getValue()+"\r\n";
+                                  bbb= pattern.getObjectVar().getValue()+"\r\n";
+                                  bbbb=bb+bbb;
+                                  Files.write(Paths.get(fileName), bbbb.getBytes(), StandardOpenOption.APPEND);}
+                              else if (pattern.getPredicateVar().getValue()!= null && pattern.getSubjectVar().getValue()!= null && pattern.getObjectVar().getValue()== null){
+                                    b= pattern.getPredicateVar().getValue()+"\r\n";
+                                    bb= pattern.getSubjectVar().getValue()+"\r\n";
+                                    bbbb=bb;
+                                    Files.write(Paths.get(fileName), bbbb.getBytes(), StandardOpenOption.APPEND);}
+                              else if (pattern.getPredicateVar().getValue()!= null && pattern.getSubjectVar().getValue()== null && pattern.getObjectVar().getValue()!= null){
+                                    b= pattern.getPredicateVar().getValue()+"\r\n";
+                                    bb= pattern.getObjectVar().getValue()+"\r\n";
+                                    bbbb=bb;
+                                    Files.write(Paths.get(fileName), bbbb.getBytes(), StandardOpenOption.APPEND);}
+                              else if (pattern.getPredicateVar().getValue()== null && pattern.getSubjectVar().getValue()!= null && pattern.getObjectVar().getValue()!= null){
+                                    b= pattern.getSubjectVar().getValue()+"\r\n";
+                                    bb= pattern.getObjectVar().getValue()+"\r\n";
+                                    bbbb=b+bb;
+                                    Files.write(Paths.get(fileName), bbbb.getBytes(), StandardOpenOption.APPEND);}
+                              else if (pattern.getPredicateVar().getValue()== null && pattern.getSubjectVar().getValue()== null && pattern.getObjectVar().getValue()!= null){
+                                    bb= pattern.getObjectVar().getValue()+"\r\n";
+                                    bbbb=bb;
+                                    Files.write(Paths.get(fileName), bbbb.getBytes(), StandardOpenOption.APPEND);}
+                              else if (pattern.getPredicateVar().getValue()== null && pattern.getSubjectVar().getValue()!= null && pattern.getObjectVar().getValue()== null){
+                                    b= pattern.getSubjectVar().getValue()+"\r\n";
+                                    bbbb=b;
+                                    Files.write(Paths.get(fileName), bbbb.getBytes(), StandardOpenOption.APPEND);}
+                              else if (pattern.getPredicateVar().getValue()!= null && pattern.getSubjectVar().getValue()== null && pattern.getObjectVar().getValue()== null){
+                                    b= pattern.getPredicateVar().getValue()+"\r\n";
+                                    
+                                    bbbb=b;}
+                                    // Files.write(Paths.get(fileName), bbbb.getBytes(), StandardOpenOption.APPEND);}
+                                  }}
 
 		} catch (Exception e) {
             e.printStackTrace();
