2a0612e4f1b4ecc36d8825cfdd0d801f40380068
Maryam <m.mohamadi898@yahoo.com>
2022-12-01 01:19:34 +0000

    print only 100 top for statistics

--- visitor/src/main/java/nl/cochez/query_processing/metadata/MainStatistics.java
+++ visitor/src/main/java/nl/cochez/query_processing/metadata/MainStatistics.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.zip.GZIPInputStream;
+
+import org.apache.jena.ext.com.google.common.collect.Iterables;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.graph.impl.LiteralLabel;
@@ -17,8 +19,10 @@ import org.apache.jena.sparql.algebra.op.OpBGP;
 
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
 
 public class MainStatistics {
 
@@ -31,6 +35,7 @@ public class MainStatistics {
 		Multiset<String> literal_labels = HashMultiset.create();
 		Multiset<String> languages = HashMultiset.create();
 		Multiset<String> types = HashMultiset.create();
+		Multiset<String> rdf_types = HashMultiset.create();
 
 		@Override
 		public void visit(OpBGP opBGP) {
@@ -45,7 +50,7 @@ public class MainStatistics {
 				}
 				Node p = triple.getPredicate();
 				if (p.isURI()) {
-					predicates.add(s.getURI());
+					predicates.add(p.getURI());
 				} else if (p.isVariable()) {
 					// TODO
 				} else {
@@ -53,7 +58,7 @@ public class MainStatistics {
 				}
 				Node o = triple.getObject();
 				if (o.isURI()) {
-					objects.add(s.getURI());
+					objects.add(o.getURI());
 				} else if (o.isVariable()) {
 					// TODO
 				} else if (o.isLiteral()) {
@@ -75,6 +80,15 @@ public class MainStatistics {
 				} else {
 					// blank nodes ingored
 				}
+				
+				// find all RDF types
+				
+				if (p.isURI() && p.getURI().equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
+					if (o.isURI()) {
+						String rdf_type = o.getURI();
+						rdf_types.add(rdf_type);
+					}
+				}
 			}
 
 		}
@@ -82,28 +96,50 @@ public class MainStatistics {
 
 	public static void main(String[] args) throws IOException {
 		Stopwatch watch = Stopwatch.createStarted();
-        org.apache.jena.query.ARQ.init();
-		InputStream in = new FileInputStream("/home/jovyan/work/persistent/visitor/src/main/java/nl/cochez/query_processing/2017-06-12_2017-07-09_organic.tsv.gz");
+		String filename = "/home/coder/project/visitor/src/main/java/nl/cochez/query_processing/metadata/drugbank_test.tsv.gz";
+		if (args.length > 0) {
+			filename = args[0];
+		}
+		
+		InputStream in = new FileInputStream(filename);
 
 		final StatVisitor visitor = new StatVisitor();
-
+		
 		IQueryCollector collector = new IQueryCollector() {
+			int failures = 0;
 
+//			@Override
+//			public void stats() {
+//				System.out.println("subjects " +  Iterables.limit(Multisets.copyHighestCountFirst(visitor.subjects).entrySet(), 100));
+//				System.out.println("predicates " + visitor.predicates);
+//				System.out.println("objects " + visitor.objects);
+//				System.out.println("literals" + visitor.literal_values);
+//				System.out.println("languages" + visitor.languages);
+//				System.out.println("types" + visitor.types);
+//				System.out.println("literal_labels" + visitor.literal_labels);
+//				System.out.println("rdf_types" + Multisets.copyHighestCountFirst(visitor.rdf_types));
+//				
+//				System.out.println("Number of failures is : " + failures);
+//			}
 			@Override
 			public void stats() {
-				System.out.println(visitor.subjects);
-				System.out.println(visitor.predicates);
-				System.out.println(visitor.objects);
-				System.out.println(visitor.literal_values);
-				System.out.println(visitor.languages);
-				System.out.println(visitor.types);
-				System.out.println(visitor.literal_labels);
+				System.out.println("subjects " +  Iterables.limit(Multisets.copyHighestCountFirst(visitor.subjects).entrySet(), 100));
+				System.out.println("predicates " + Iterables.limit(Multisets.copyHighestCountFirst(visitor.predicates).entrySet(), 100));
+				System.out.println("objects " + Iterables.limit(Multisets.copyHighestCountFirst(visitor.objects).entrySet(), 100));
+				System.out.println("literals" + Iterables.limit(Multisets.copyHighestCountFirst(visitor.literal_values).entrySet(), 100));
+				System.out.println("languages" + Iterables.limit(Multisets.copyHighestCountFirst(visitor.languages).entrySet(), 100));
+				System.out.println("types" + Iterables.limit(Multisets.copyHighestCountFirst(visitor.types).entrySet(), 100));
+				System.out.println("literal_labels" + Iterables.limit(Multisets.copyHighestCountFirst(visitor.literal_labels).entrySet(), 100));
+				System.out.println("rdf_types" + Iterables.limit(Multisets.copyHighestCountFirst(visitor.rdf_types).entrySet(), 100));
+				
+				System.out.println("Number of failures is : " + failures);
 			}
 
 			@Override
 			public void reportFailure(String input) {
 //				throw new RuntimeException("");
-				// System.err.println("ignoring" + input);
+				//System.err.println("ignoring " + input);
+				failures++;
 			}
 
 			@Override
@@ -115,15 +151,16 @@ public class MainStatistics {
 		};
 
 		IterateQueriesFromWikidataLog.processFromFile(new GZIPInputStream(in),collector);
-		
-		// List<String> queries = Lists.newArrayList("SELECT * WHERE { <http://test.com/subject> ?p \"Hello\" }", "SELECT * WHERE { <http://test.com/subject> ?p 5 }",
-		// 		"SELECT * WHERE { <http://test.com/subject> ?p \"Hello\"@en }");
-		// for (String query : queries) {
-		// 	Query q = QueryFactory.create(query);
-		// 	System.out.println(q);
-		// 	Op op = Algebra.compile(q);
-		// 	op.visit(visitor);
-		// }
+
+//		List<String> queries = Lists.newArrayList("SELECT * WHERE { <http://test.com/subject> a <https://www.wikidata.org/wiki/Q5> }");
+//				//, "SELECT * WHERE { <http://test.com/subject> ?p 5 }",
+////			"SELECT * WHERE { <http://test.com/subject> ?p \"Hello\"@en }");
+//		for (String query : queries) {
+//			Query q = QueryFactory.create(query);
+//			System.out.println(q);
+//			Op op = Algebra.compile(q);
+//			op.visit(visitor);
+//		}
 		watch.stop();
 		System.out.println("Elapsed" + watch.elapsed(TimeUnit.SECONDS));
 
